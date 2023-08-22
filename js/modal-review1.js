8ea293f37362f3a8e803d9f0c04f1689f1b922b3
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2023-08-22 17:34:14 +0300

    2

--- js/modal-review1.js
+++ js/modal-review1.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const phoneNumberInput = document.getElementById("phone");
+  let isTyping = false; // Folosit pentru a verifica dacă utilizatorul este în timpul tastării
 
   // Inițializați intlTelInput
   const iti = window.intlTelInput(phoneNumberInput, {
@@ -20,11 +21,44 @@ document.addEventListener("DOMContentLoaded", () => {
     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.3/js/utils.js",
   });
 
-  // Setați stilul pentru padding-left direct pe elementul de input
-  phoneNumberInput.style.paddingLeft = "177px";
+  // Stabiliți valorile de padding în funcție de latimea ecranului
+  function updatePaddingBasedOnScreenWidth() {
+    if (window.innerWidth <= 767) {
+      phoneNumberInput.style.paddingLeft = "106px";
+    } else {
+      phoneNumberInput.style.paddingLeft = "177px";
+    }
+  }
+
+  // Ascultați evenimentul de focus pentru a detecta când utilizatorul începe să tasteze
+  phoneNumberInput.addEventListener("focus", () => {
+    isTyping = true;
+  });
+
+  // Ascultați evenimentul de blur pentru a detecta când utilizatorul a ieșit din câmpul de număr de telefon
+  phoneNumberInput.addEventListener("blur", () => {
+    isTyping = false;
+    updatePaddingBasedOnScreenWidth(); // Actualizați padding-ul atunci când utilizatorul iese din câmp
+  });
 
   // Asigurați-vă că valoarea de număr de telefon este corectă atunci când se modifică prefixul
   phoneNumberInput.addEventListener("change", () => {
-    phoneNumberInput.value = iti.getNumber();
+    const numberWithoutPrefix = iti.getNumber(intlTelInputUtils.numberFormat.NATIONAL);
+
+    if (numberWithoutPrefix.startsWith(`+${iti.getSelectedCountryData().dialCode}`)) {
+      phoneNumberInput.value = numberWithoutPrefix.slice(iti.getSelectedCountryData().dialCode.length + 1);
+    } else {
+      phoneNumberInput.value = numberWithoutPrefix;
+    }
   });
+
+  // Actualizați valorile de padding la redimensionarea ferestrei
+  window.addEventListener("resize", () => {
+    if (!isTyping) {
+      updatePaddingBasedOnScreenWidth(); // Actualizați padding-ul numai dacă nu este în timpul tastării
+    }
+  });
+
+  // Inițializați padding-ul la încărcarea paginii
+  updatePaddingBasedOnScreenWidth();
 });
